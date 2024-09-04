2d8901602b6bfb70b5092a1f7511ed20f1ec51e9
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-04 20:50:01 +0530

    update varsity

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -9,11 +9,11 @@ const Pricing = () => {
                         <h2 className='text-4xl mb-5 text-[#424242] font-semibold gap-2 mb-10'>Unbeatable pricing</h2>
                         <p className='flex  mb-6 leading-loose text-[#666] gap-2'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
                     </div>
-                    <div className='flex'>
+                    <div className='flex box-border'>
                         <div className='flex'>
-                            <img className='w-full' src="https://zerodha.com/static/images/pricing-eq.svg" alt='price-0' />
-                            <p>Free account
-                                opening</p>
+                            <img className='w-[240px] max-w-full' src="https://zerodha.com/static/images/pricing-eq.svg" alt='price-0' />
+                            <div className=' flex text-[12px] w-[90px]  mt-24 -ml-5   text-[#666] '>Free account <br/>
+                            opening </div>
                         </div>
                         <div className='flex'>
                             <img className='w-full' src="https://zerodha.com/static/images/pricing-eq.svg" alt="price-0" />
--- src/components/Varsity.jsx
+++ src/components/Varsity.jsx
@@ -4,22 +4,7 @@ import varsity from '../assets/index-education.png'
 const Varsity = () => {
   return (
     <div className='flex'>
-      <div className='flex'>
-        <img src={varsity} alt="index-education" />
-      </div>
-      <div className='flex flex-col'>
-       <h2>Free and open market education</h2>
-       <div className='flex flex-col'>
-       <div>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</div>
-       <p>Varsity</p>
-       {/* <img src={rightarrow} alt="" /> */}
-       </div>
-       <div className='flex flex-col'>
-        <div>TradingQ&A, the most active trading and investment community in India for all your market related queries.</div>
-        <p>Trending Q&A</p>
-        {/* <img src={rightarrow} alt="" /> */}
-       </div>
-      </div>
+      
     </div>
   )
 }
